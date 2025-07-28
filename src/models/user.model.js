319a5e4a19b6bf49b424d665ff5fbb3990c3a287
Hassanali1112 <134091195+Hassanali1112@users.noreply.github.com>
2025-07-28 23:50:52 +0500

    User model added

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose,{Schema} from "mongoose";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
 const UserSchema = new Schema({
   userName : {
@@ -45,6 +47,43 @@ const UserSchema = new Schema({
   {
     timestamps : true,
   })
+
+UserSchema.pre("save", async function(next){
+  if (!this.isModified("password")) return;
+  this.password = bcrypt.hash(this.password, 10)
+  next()
+})
+
+UserSchema.methods.isPasswordCorrect = async function (password){
+ return await bcrypt.compare(password, this.password)
+}
+
+UserSchema.methods.generateAccessToken = async function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      userName: this.userName,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+}
+
+UserSchema.methods.generateRefreshToken = async function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
 
   export const User = mongoose.model("User", UserSchema)
 
